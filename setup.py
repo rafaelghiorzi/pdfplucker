5b2aaeda218bc4a8176f3dd2d87a04a337b5781b
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2025-04-09 22:53:58 -0300

    batman

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Read requirements
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().strip().split("\n")
+
+setup(
+    name="pdf-processor",
+    version="0.1.0",
+    description="Extrator e processador de documentos PDF utilizando Docling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Rafael Dias Ghiorzi",
+    author_email="rafael.ghiorzi@gmail.com",
+    url="sem github ainda",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: Portuguese (Brazilian)"
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pdf-processor=pdf_processor.cli:main",
+        ],
+    },
+)
