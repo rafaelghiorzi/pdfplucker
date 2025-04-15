cefb7c4b09d02056ad562e75104e65c8ed5a3a1e
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2025-04-14 22:25:21 -0300

    always the toml

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 pdfplucker - a powerful wrapper for the Docling library
 """
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 from processor import process_batch, process_pdf, create_converter
 from utils import format_result, link_subtitles
