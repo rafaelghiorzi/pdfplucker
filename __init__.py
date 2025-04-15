5f5a52ab96aa82ff9b66697640d7cc87139104ac
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2025-04-14 22:22:33 -0300

    .

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 pdfplucker - a powerful wrapper for the Docling library
 """
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 
 from processor import process_batch, process_pdf, create_converter
 from utils import format_result, link_subtitles
